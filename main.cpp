fd559134ba9cc9b84f8a0a074a2bbeff41144404
Accelerator <3170347411@qq.com>
2023-09-24 10:58:03 +0800

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
+class hello_world
+{
+private:
+    string print;
+public:
+    hello_world()
+    {
+        print = "I'm Accelerator. Hello, World!";
+    }
+    void out()
+    {
+        cout << print << endl;
+    }
+};
 int main() {
-
-    std::cout << "I'm Accelerator. Hello, World!" << std::endl;
+    hello_world hello;
+    hello.out();
     return 0;
 }
