521dfbb1d5886b8a121abc1763d6a448eff586c8
Accelerator <3170347411@qq.com>
2023-10-07 20:43:21 +0800

    last

--- main.cpp
+++ main.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-    cout << "My name is Accelerator, hello world" << endl;
-=======
     cout << "hello world" << endl;
->>>>>>> d1246fb2dae24cb7314392dac0f045fdc343cc3b
     return 0;
 }
